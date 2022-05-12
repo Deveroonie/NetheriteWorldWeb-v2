705bb89d9c65497e25dbfddc62d0ff84f280903d
SirEpiclyAmazing <sirepiclydev@gmail.com>
2022-05-12 18:03:26 +0100

    Add the server online player count to the home page

--- js/getServerOnlinePlayerCount.js
+++ js/getServerOnlinePlayerCount.js
@@ -3,5 +3,6 @@ const ip = 'play.netherite.world'
 function getPlayers() {
     axios.get(`https://api.mcsrvstat.us/2/${ip}`).then(function (response) {
         console.log(response.data.players.online)
+        document.getElementById("mcOnlinePlayers").innerText = response.data.players.online
     })
 }
